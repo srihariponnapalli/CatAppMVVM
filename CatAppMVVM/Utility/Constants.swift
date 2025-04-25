4155e2e9d6c6a69b108df1c5007158cc9a7ea282
Srihari <srihariponnapalli@gmail.com>
2025-04-25 15:28:14 +0800

    Constans file added

---
+++ CatAppMVVM/Utility/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CatAppMVVM
+//
+//  Created by user260574 on 4/25/25.
+//
+
+
+import Foundation
+
+struct Constants {
+    struct URLs {
+        static let catFactURL = "https://meowfacts.herokuapp.com"
+        static let catImageURL = "https://api.thecatapi.com/v1/images/search"
+    }
+}
